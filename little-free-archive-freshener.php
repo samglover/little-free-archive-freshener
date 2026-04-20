2b84c3e34e7f43cc100d23b1098b398b0e4439b5
samglover <sam@samglover.net>
2026-04-20 10:39:40 -0500

    Address WPCS errors and warnings

--- little-free-archive-freshener.php
+++ little-free-archive-freshener.php
@@ -1,47 +1,61 @@
 <?php
-
-/*
-Plugin Name: Little Free Archive Freshener
-Plugin URI: https://samglover.net/projects/little-free-archive-freshener/
-Description: Little Free Archive Freshener is a helpful dashboard widget that prompts you to update your archives, one post or page at a time.
-Author: Sam Glover
-Version: 1.0.2
-Author URI: https://samglover.net
-*/
+/**
+ * Plugin Name: Little Free Archive Freshener
+ * Plugin URI: https://samglover.net/projects/little-free-archive-freshener/
+ * Description: Little Free Archive Freshener is a helpful dashboard widget that prompts you to update your archives, one post or page at a time.
+ * Author: Sam Glover
+ * Version: 1.0.3
+ * Author URI: https://samglover.net
+ *
+ * @file    little-free-archive-freshener.php
+ * @package LFAF
+ * @since   1.0.0
+ */
 
 namespace ARCHIVE_FRESHENER;
 
-if (!defined('ABSPATH')) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Constants
  */
-define('ARCHIVE_FRESHENER_PLUGIN_VERSION', '1.0.2');
-define('ARCHIVE_FRESHENER_DIR_PATH', plugin_dir_path(__FILE__));
-define('ARCHIVE_FRESHENER_DIR_URL', plugin_dir_url(__FILE__));
+define( 'ARCHIVE_FRESHENER_PLUGIN_VERSION', '1.0.2' );
+define( 'ARCHIVE_FRESHENER_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'ARCHIVE_FRESHENER_DIR_URL', plugin_dir_url( __FILE__ ) );
 
 
 /**
- * Plugin Files
+ * Include plugin files
  */
-if (is_admin()) {
-  require_once(ARCHIVE_FRESHENER_DIR_PATH . 'widget.php');
-  require_once(ARCHIVE_FRESHENER_DIR_PATH . 'options.php');
+if ( is_admin() ) {
+	require_once ARCHIVE_FRESHENER_DIR_PATH . 'widget.php';
+	require_once ARCHIVE_FRESHENER_DIR_PATH . 'options.php';
 }
 
-add_action('admin_enqueue_scripts', __NAMESPACE__ . '\register_admin_js');
+
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\register_admin_js' );
+/**
+ * Enqeue javascript
+ */
 function register_admin_js() {
-	wp_enqueue_script('wpau-admin-js', ARCHIVE_FRESHENER_DIR_URL . 'admin.js', ['jquery'], '', true);
+	wp_enqueue_script( 'wpau-admin-js', ARCHIVE_FRESHENER_DIR_URL . 'admin.js', array( 'jquery' ), ARCHIVE_FRESHENER_PLUGIN_VERSION, true );
 }
 
-register_activation_hook(__FILE__, __NAMESPACE__ . '\activate');
+register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
+/**
+ * Set default options on plugin activation
+ */
 function activate() {
-  $default_options = [
-		'lfaf_expiration_date'      => 90,
-    'lfaf_included_post_types'  => ['post', 'page'],
-  ];
+	$default_options = array(
+		'lfaf_expiration_date'     => 90,
+		'lfaf_included_post_types' => array( 'post', 'page' ),
+	);
 
-  foreach ($default_options as $key => $val) {
-    if (!get_option($key)) update_option($key, $val);
-  }
+	foreach ( $default_options as $key => $val ) {
+		if ( ! get_option( $key ) ) {
+			update_option( $key, $val );
+		}
+	}
 }
--- options.php
+++ options.php
@@ -1,88 +1,158 @@
 <?php
+/**
+ * Options
+ *
+ * @file options.php
+ * @package LFAF
+ * @since 1.0.0
+ */
 
 namespace ARCHIVE_FRESHENER;
 
-add_action('admin_init', __NAMESPACE__ . '\options_init');
+add_action( 'admin_init', __NAMESPACE__ . '\options_init' );
+/**
+ * Initialize options
+ */
 function options_init() {
-  add_settings_section(
-    'lfaf-options',
-    __('Little Free Archive Freshener Options', 'little-free-archive-freshener'),
-    __NAMESPACE__ . '\lfaf_options_section',
-    'writing',
-  );
+	add_settings_section(
+		'lfaf-options',
+		__( 'Little Free Archive Freshener Options', 'little-free-archive-freshener' ),
+		__NAMESPACE__ . '\lfaf_options_section',
+		'writing',
+	);
 
-  register_setting('writing', 'lfaf_expiration_date');
-  add_settings_field(
-    'lfaf_expiration_date',
-    '<label for="lfaf_expiration_date">' . __('Freshen After', 'little-free-archive-freshener') . '</label>',
-    __NAMESPACE__ . '\lfaf_expiration_date',
-    'writing',
-    'lfaf-options',
-  );
+	register_setting( 'writing', 'lfaf_expiration_date' );
+	/**
+	 * Set expiration date
+	 */
+	add_settings_field(
+		'lfaf_expiration_date',
+		'<label for="lfaf_expiration_date">' . __( 'Freshen After', 'little-free-archive-freshener' ) . '</label>',
+		__NAMESPACE__ . '\lfaf_expiration_date',
+		'writing',
+		'lfaf-options',
+	);
 
-  register_setting('writing', 'lfaf_included_post_types');
-  add_settings_field(
-    'lfaf_included_post_types',
-    '<label for="lfaf_included_post_types">' . __('Include Post Types', 'little-free-archive-freshener') . '</label>',
-    __NAMESPACE__ . '\lfaf_included_post_types',
-    'writing',
-    'lfaf-options',
-  );
+	register_setting( 'writing', 'lfaf_included_post_types' );
+	/**
+	 * Select post types
+	 */
+	add_settings_field(
+		'lfaf_included_post_types',
+		'<label for="lfaf_included_post_types">' . __( 'Include Post Types', 'little-free-archive-freshener' ) . '</label>',
+		__NAMESPACE__ . '\lfaf_included_post_types',
+		'writing',
+		'lfaf-options',
+	);
 
-  register_setting('writing', 'lfaf_clear_ignored');
-  add_settings_field(
-    'lfaf_clear_ignored',
-    '<label for="lfaf_clear_ignored">' . __('Clear Ignored Posts', 'little-free-archive-freshener') . '</label>',
-    __NAMESPACE__ . '\lfaf_clear_ignored',
-    'writing',
-    'lfaf-options',
-  );
+	register_setting( 'writing', 'lfaf_clear_ignored' );
+	/**
+	 * Clear ignored posts
+	 */
+	add_settings_field(
+		'lfaf_clear_ignored',
+		'<label for="lfaf_clear_ignored">' . __( 'Clear Ignored Posts', 'little-free-archive-freshener' ) . '</label>',
+		__NAMESPACE__ . '\lfaf_clear_ignored',
+		'writing',
+		'lfaf-options',
+	);
 }
 
 
+/**
+ * Options section
+ */
 function lfaf_options_section() {
-  echo '<p>' . __('Options for the Freshen Up Your Archives dashboard widget.', 'little-free-archive-freshener') . '</p>';
+	?>
+	<p>
+		<?php esc_html_e( 'Options for the Freshen Up Your Archives dashboard widget.', 'little-free-archive-freshener' ); ?>
+	</p>
+	<?php
 }
 
 
+/**
+ * Expiration date field
+ */
 function lfaf_expiration_date() {
-  $expiration_date = get_option('lfaf_expiration_date');
-  echo '<input name="lfaf_expiration_date" class="small-text" type="number" required aria-required="true" value="' . $expiration_date . '"> ' . __('days', 'little-free-archive-freshener');
-  echo '<p class="description">' . sprintf(__('Items older than this will be fetched. Items that have been updated within the last %1$s days are "fresh" and will not be fetched.', 'little-free-archive-freshener'), $expiration_date) . '</p>';
+	$expiration_date = get_option( 'lfaf_expiration_date' );
+	?>
+	<input 
+		name="lfaf_expiration_date" 
+		class="small-text" 
+		type="number" 
+		required aria-required="true" 
+		value="<?php echo esc_html( $expiration_date ); ?>"
+	>
+	<?php esc_html_e( 'days', 'little-free-archive-freshener' ); ?>
+	<p class="description">
+		<?php
+		echo esc_html(
+			sprintf(
+				// Translators: %s is the current expiration date value.
+				__( 'Items older than this will be fetched. Items that have been updated within the last %s days are "fresh" and will not be fetched.', 'little-free-archive-freshener' ),
+				$expiration_date
+			)
+		);
+		?>
+	</p>
+	<?php
 }
 
+/**
+ * Included post types checkboxes
+ */
 function lfaf_included_post_types() {
-  $post_types           = get_post_types(['public' => true]);
-  $included_post_types  = get_option('lfaf_included_post_types');
-
-  ob_start();
-    echo '<fieldset>';
-      foreach ($post_types as $key => $post_type) {
-        if (in_array($post_type, $included_post_types)) {
-          $checked = ' checked';
-        } else {
-          $checked = '';
-        }
-        echo '<p><label for="lfaf_included_post_types[' . $key . ']">';
-          echo '<input name="lfaf_included_post_types[' . $key . ']" id="lfaf_included_post_types[' . $key . ']" type="checkbox" value="' . $post_type . '"' . $checked . '>';
-          echo $post_type;
-        echo '</label></p>';
-
-        unset($checked);
-      }
-    echo '</fieldset>';
-  echo ob_get_clean();
+	$post_types          = get_post_types( array( 'public' => true ) );
+	$included_post_types = get_option( 'lfaf_included_post_types' );
+	?>
+	<fieldset>
+		<?php foreach ( $post_types as $key => $post_type ) { ?>
+			<?php
+			if ( in_array( $post_type, $included_post_types, true ) ) {
+				$checked = ' checked';
+			} else {
+				$checked = '';
+			}
+			?>
+			<p>
+				<label for="lfaf_included_post_types[<?php echo esc_attr( $key ); ?>]">
+					<input 
+						name="lfaf_included_post_types[<?php echo esc_attr( $key ); ?>]"
+						id="lfaf_included_post_types[<?php echo esc_attr( $key ); ?>]"
+						type="checkbox" 
+						value="<?php esc_attr( $post_type ); ?>"
+						<?php echo esc_attr( $checked ); ?>
+					>
+					<?php echo esc_html( $post_type ); ?>
+				</label>
+			</p>
+			<?php unset( $checked ); ?>
+		<?php } ?>
+	</fieldset>
+	<?php
 }
 
 
+/**
+ * Clear ignored posts checkbox
+ */
 function lfaf_clear_ignored() {
-  ob_start();
-    echo '<fieldset>';
-      echo '<p><label for="lfaf_clear_ignored">';
-        echo '<input name="lfaf_clear_ignored" id="lfaf_clear_ignored" type="checkbox" value="1">';
-        _e('Clear the list of ignored posts.', 'little-free-archive-freshener');
-      echo '</label></p>';
-      echo '<p class="description">' . __('If you check this box and then save changes, it will clear the list of items for which you have previously clicked "ignore forever." This box will not remain checked after you save changes.', 'little-free-archive-freshener') . '</p>';
-    echo '</fieldset>';
-  echo ob_get_clean();
+	?>
+	<fieldset>
+		<p>
+			<label for="lfaf_clear_ignored">
+				<input
+					name="lfaf_clear_ignored" 
+					id="lfaf_clear_ignored" 
+					type="checkbox" value="1"
+				>
+				<?php esc_html_e( 'Clear the list of ignored posts.', 'little-free-archive-freshener' ); ?>
+			</label>
+		</p>
+		<p class="description">
+			<?php esc_html_e( 'If you check this box and then save changes, it will clear the list of items for which you have previously clicked "ignore forever." This box will not remain checked after you save changes.', 'little-free-archive-freshener' ); ?>
+		</p>
+	</fieldset>
+	<?php
 }
